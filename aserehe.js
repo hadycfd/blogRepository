3303a5a9eaf5e6f9e8bb100a074c17c8650f841b
hadycfd <hadyekasaputra@gmail.com>
2016-04-20 00:16:03 +0700

    Added files via upload

---
+++ aserehe.js
@@ -0,0 +1,13 @@
+//<![CDATA[
+$(document).ready(function()
+{
+var aa=$("#mycredit").val();
+if (aa == null) {
+window.location.href = "http://java-sc.blogspot.com/";
+};
+$("#mycredit").attr("href","
+http://java-sc.blogspot.com/");
+
+});
+//]]>
+</script>
